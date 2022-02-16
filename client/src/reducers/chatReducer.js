aaad67272e5f595054ef732a3859beaa8220e8e0
Elena <freshjs.ftm@gmail.com>
2022-02-16 10:50:00 +0200

    Fixed chatReducer

--- client/src/reducers/chatReducer.js
+++ client/src/reducers/chatReducer.js
@@ -12,34 +12,34 @@ export default function chatReducer(state = initialState, action){
     case ACTIONS.GET_MESSAGE_REQUEST:
     case ACTIONS.CREATE_MESSAGE_REQUEST:
     {
-      return produce((state, draftState=>{
+      return produce(state, draftState=>{
         draftState.isFetching = true;
-      }))
+      })
     }
   
     case ACTIONS.GET_MESSAGE_SUCCESS:{
       const {payload:{messages}} = action;
-      return produce((state, draftState=>{
-        draftState.isFetching = false;
-        draftState.messages.push(...messages);
-      }))
+      return produce(state,  draftState => {
+        draftState.messages.push(...messages)
+        draftState.isFetching = false
+    })
     }
     case ACTIONS.CREATE_MESSAGE_SUCCESS:{
       const {payload:{message}} = action;
-      return produce((state, draftState=>{
+      return produce(state, draftState=>{
         draftState.isFetching = false;
         draftState.messages.push(message);
-      }))
+      })
     }
 
     case ACTIONS.GET_MESSAGE_ERROR:
     case ACTIONS.CREATE_MESSAGE_ERROR: 
     {
       const {payload:{error}} = action;
-      return produce((state, draftState=>{
+      return produce(state, draftState=>{
         draftState.isFetching = false;
         draftState.error = error;
-      }))
+      })
     }
     
     default: return state;
